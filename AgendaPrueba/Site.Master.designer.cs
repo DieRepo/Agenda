459a935b9f258ab1e913e11fbf28eb6a62e56dde
DieRepo <107214447+DieRepo@users.noreply.github.com>
2022-10-10 13:15:10 -0500

    menu

--- AgendaPrueba/Site.Master.designer.cs
+++ AgendaPrueba/Site.Master.designer.cs
@@ -11,8 +11,26 @@ namespace AgendaPrueba
 {
 
 
-    public partial class SiteMaster
+   public partial class SiteMaster
     {
+
+        /// <summary>
+        /// Control head.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.ContentPlaceHolder head;
+
+        /// <summary>
+        /// Control menu.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Menu menu;
 
         /// <summary>
         /// Control MainContent.

7c25bf8a47bb2ae76f563d7bbda5085531b75e41
KaRy PiÑa <zilkar13@hotmail.es>
2022-10-12 11:41:30 -0500

    agregar contac.aspx

--- AgendaPrueba/Contact.aspx.designer.cs
+++ AgendaPrueba/Contact.aspx.designer.cs
@@ -22,23 +22,5 @@ namespace AgendaPrueba
         /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
         /// </remarks>
         protected global::System.Web.UI.WebControls.Panel panelPDF;
-
-        /// <summary>
-        /// Control iFramePdf.
-        /// </summary>
-        /// <remarks>
-        /// Campo generado automáticamente.
-        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
-        /// </remarks>
-        protected global::System.Web.UI.HtmlControls.HtmlIframe iFramePdf;
-
-        /// <summary>
-        /// Control yourIframe.
-        /// </summary>
-        /// <remarks>
-        /// Campo generado automáticamente.
-        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
-        /// </remarks>
-        protected global::System.Web.UI.HtmlControls.HtmlIframe yourIframe;
     }
 }
